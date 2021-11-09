afd40be1de04b795c20712279e5be9b0dc130891
Gabriel Duete <gabrielmonteiroduete@gmail.com>
2021-11-09 19:36:44 -0300

    Configurando envio de mensagem no email da API staticforms

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -3,20 +3,22 @@ import './style.css'
 
 function Form(){
    return (
-        <form className = 'containerForm' action = 'https://api.staticforms.xyz/submit' method = 'POST' encType = 'text/plain'>
-            <input type="hidden" name="accessKey" value = "7caa4f10-d692-492c-9aa8-3845a46aa7f8" />
+        <form className = 'containerForm' action="https://api.staticforms.xyz/submit" method="post" >
+    
+            <input type="hidden" name="accessKey" value="7caa4f10-d692-492c-9aa8-3845a46aa7f8" />
 
             <div className = 'inputs'>
-                <input type = "text" name = "name" className = 'name' placeholder = "Name" required />
-                <input type = "email" name = "email" className = "email" placeholder = "Email" required/>
+                <input type="text" name="name" placeholder = 'Name' required/> 
+                <input type="text" name="email" placeholder  = 'E-mail' required/>
             </div>
             
-            <textarea name = "message" cols="30" rows="10" placeholder = 'Message' required></textarea>
+            <textarea name="message" cols="30" rows="10" placeholder = 'Send a message' required></textarea>
 
-            <p></p>
-        
-            <button type = "submit">Submit</button>
+            <input type="hidden" name="replyTo" value="@" />
+
+            <button type="submit" value="Submit"> Submit </button>
         </form>
+        
     )
 }
 
