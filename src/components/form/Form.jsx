94076262f53d770bbcf5266d9ea964258b759136
Gabriel Duete <gabrielmonteiroduete@gmail.com>
2021-11-09 19:56:42 -0300

    Colocando pro form redirecionar para a mesma página

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,22 +1,24 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './style.css'
 
 function Form(){
-   return (
+
+    return (
         <form className = 'containerForm' action="https://api.staticforms.xyz/submit" method="post" >
-    
-            <input type="hidden" name="accessKey" value="7caa4f10-d692-492c-9aa8-3845a46aa7f8" />
+            
+            {/* Inputs ocultos para configuração da API Static Forms */}
+            <input type = "hidden" name = "accessKey" value = "7caa4f10-d692-492c-9aa8-3845a46aa7f8" />
+            <input type = "hidden" name = "redirectTo" value = {window.location.toString()} />
+            <input type = "hidden" name = "replyTo" value="@" />
 
             <div className = 'inputs'>
-                <input type="text" name="name" placeholder = 'Name' required/> 
-                <input type="text" name="email" placeholder  = 'E-mail' required/>
+                <input type = "text" name = "name" placeholder = 'Name' required/> 
+                <input type = "email" name = "email" placeholder  = 'E-mail' required/>
             </div>
             
-            <textarea name="message" cols="30" rows="10" placeholder = 'Send a message' required></textarea>
+            <textarea name = "message" cols = "30" rows="10" placeholder = 'Send a message' required></textarea>
 
-            <input type="hidden" name="replyTo" value="@" />
-
-            <button type="submit" value="Submit"> Submit </button>
+            <button type = "submit" value = "Submit"> Submit </button>
         </form>
         
     )
