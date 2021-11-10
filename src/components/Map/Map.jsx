939dd7ae06e90e0cef65c60376be2aa905e5bd73
Gabriel Duete <gabrielmonteiroduete@gmail.com>
2021-11-09 21:58:07 -0300

    Updates e inicialização do componente do mapa

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Mapp(){
+    return (
+        <>
+           <h4> MAPA </h4> 
+        </>
+    )
+}
+
+export default Mapp
--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './style.css'
 import Form from '../form/Form'
+import Mapp from '../Map/Map'
 
 function Contact(){
     return (
         <article>
             <h3>Contact <span>Us</span></h3>
             <p className = 'pContact'>Localization</p>
+            <Mapp />
             <p className = 'pContact'>Send a message</p>
             <Form />
         </article>
