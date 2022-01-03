536a4f8bed9837983e1706b6cb99b38b262ebc38
Gabriel Duete <gabrielmonteiroduete@gmail.com>
2022-01-03 14:42:18 -0300

    oxi

--- src/Pages/pageFavs/FavsDrinks.jsx
+++ src/Pages/pageFavs/FavsDrinks.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import './pageFavs.css'
+import Remove from '../../components/remove/Remove'
 
-function FavDrinks(props){
+function FavDrinks(props) {
     return (
         <>
-            <div className = 'drinkShop'>
-                    <img src = {props.image} alt="image_cocktail" />
+            <div className='drinkShop'>
+                <img src={props.image} alt="image_cocktail" />
+                <div className='drinkShop-texts'>
                     <p>{props.nameDrink}</p>
+                    <Remove 
+                        removeDrink={props.removeDrink}
+                        setArrayFavs={props.setArrayShop}
+                        nameDrink1={props.nameDrink}
+                        array={props.arrayFavs}
+                    />
+                </div>
             </div>
             <hr />
         </>
